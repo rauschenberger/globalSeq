a978e54d86530fdad6eeae0a104cc97037687702
rauschenberger <rauschenberger@users.noreply.github.com>
2017-12-05 11:32:44 +0100

    automation

--- toydata.R
+++ toydata.R
@@ -1,5 +1,10 @@
-# first set
+
+# This script simulates data for the vignette.
+
+ls <- ls()
 set.seed(pi)
+
+# first set
 y <- c(7,1,1,15,6,16,5,2,2,5)
 group <- c(1,1,1,2,1,2,1,1,1,1)
 offset <- c(15,1,2,28,10,28,9,2,5,7)
@@ -10,6 +15,7 @@ X[2,4:9] <- stats::rbinom(6,size = 30,prob = 0.5)
 names(y) <- paste("ind",1:length(y),sep = "")
 rownames(X) <- names(y)
 colnames(X) <- paste("X",1:ncol(X),sep = "")
+
 # second set
 Y <- matrix(stats::rnbinom(5 * 7,mu = 20,size = 1 / 0.5),nrow = 7,ncol = 5)
 Yloc <- sort(sample(1:100,size = nrow(Y)))
@@ -24,9 +30,11 @@ colnames(Y) <- colnames(V) <- colnames(W) <- paste("ind",1:5,sep = "")
 rownames(Y) <- names(Yloc) <- paste("gene",1:nrow(Y),sep = "")
 rownames(V) <- names(Vloc) <- paste("V",1:nrow(V),sep = "")
 rownames(W) <- names(Wloc) <- paste("W",1:nrow(W),sep = "")
+
 toydata <- list(
     y = y,X = X,group = group,offset = offset,
     Y = Y,Yloc = Yloc,Ychr = Ychr,V = V,Vloc = Vloc,
     Vchr = Vchr,W = W,Wloc = Wloc,Wchr = Wchr
 )
-rm(list=setdiff(ls(),c("pkg","toydata")))
+
+rm(list=setdiff(ls(),c(ls,"toydata")))
